7574b16297a2f3aae6de30abcd98c5f593572ea8
MUHUMUZAJeanPierre <j.muhumuza@alustudent.com>
2024-07-31 00:51:37 +0200

    feat/db

--- config/db.js
+++ config/db.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
 
-export const connectDB = async ()=>{
+ const connectDB = async ()=>{
     await mongoose.connect('mongodb+srv://jmuhumuza:50Iyz2y1K8JIziTK@cluster0.1aiv6ye.mongodb.net/').then(()=>console.log("Db Connected"))
 }
+
+export default connectDB;
 
 
 // mongodb://localhost:27017/FarmerConnect
